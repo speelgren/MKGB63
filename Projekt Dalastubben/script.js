c2003ab85bfebe7bbea96e5b836186afb8796be1
speelgren <97473666+speelgren@users.noreply.github.com>
2023-04-28 18:06:06 +0200

    1806

--- Projekt Dalastubben/script.js
+++ Projekt Dalastubben/script.js
@@ -1,114 +1,67 @@
 'use strict';
+/* Written in TypeScript */
 window.addEventListener('DOMContentLoaded', function () {
-    var navbar = document.querySelector('.navbar');
-    var navbarOffsetTop = navbar.offsetTop;
-    var container = document.querySelector('.container');
-    var toggleStickyNavbar = function () {
-        var _a, _b;
-        if (window.scrollY > navbarOffsetTop) {
-            (_a = document.querySelector('.hidden-nav')) === null || _a === void 0 ? void 0 : _a.classList.remove('d-none');
-            navbar.classList.add('sticky');
-        }
-        else {
-            (_b = document.querySelector('.hidden-nav')) === null || _b === void 0 ? void 0 : _b.classList.add('d-none');
-            navbar.classList.remove('sticky');
-        }
-    };
-    var setContent = function (contentFunction) {
-        /* Back-to-top */
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        /* Empty content in container */
-        container.innerHTML = '';
-        /* Add default content to container */
-        contentFunction(container);
-    };
-    /* Menu choices */
-    var contentMap = {
-        'Hem': homeContent,
-        'Om oss': aboutContent,
-        'Kontakt': contactContent,
-        'FAQ': faqContent
-    };
-    /* Create eventlistener for sticky navbar */
-    window.addEventListener('scroll', toggleStickyNavbar);
-    /* Default site content */
-    homeContent(container);
-    /* Create buttons */
-    document.querySelectorAll('.nav-btn').forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            var contentFunction = contentMap[e.target.textContent];
-            if (contentFunction) {
-                setContent(contentFunction);
-            }
+    var navBtn = document.querySelectorAll('.nav-btn');
+    var homeContainer = document.querySelector('.home-container');
+    var aboutContainer = document.querySelector('.about-container');
+    var contactContainer = document.querySelector('.contact-container');
+    var faqContainer = document.querySelector('.faq-container');
+    /* Navbar buttons. */
+    navBtn.forEach(function (button, index) {
+        button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+            navBtn.forEach(function (otherButton) {
+                if (otherButton !== button)
+                    otherButton.classList.remove('active');
+            });
+            button === null || button === void 0 ? void 0 : button.classList.add('active');
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            shownContainer(index);
         });
     });
-});
-var createCard = function (key, card) {
-    var Card = document.createElement('section');
-    Card.classList.add('card', 'm-3');
-    var CardBody = document.createElement('article');
-    CardBody.classList.add('card-body', 'scroll-item');
-    var CardImage = document.createElement('img');
-    CardImage.classList.add('card-img-top');
-    CardImage.src = card.image;
-    CardImage.alt = card.alt;
-    var CardTitle = document.createElement('h1');
-    CardTitle.classList.add('card-title');
-    CardTitle.textContent = key;
-    var CardText = document.createElement('p');
-    CardText.classList.add('card-text');
-    CardText.textContent = card.description;
-    CardBody.append(CardTitle, CardText);
-    Card.append(CardImage, CardBody);
-    return Card;
-};
-var homeContent = function (container) {
-    /* Home container */
-    var homeContainer = document.createElement('div');
-    homeContainer.classList.add('home-container', 'container', 'scroll-container');
-    /* Array */
-    var homeArray = {
-        'Stubbfräsning': {
-            'description': 'Stubbfräsning hanterar effektivt besvärliga stubbar för jämnare gräsmattor och enklare robotgräsklipparinstallation. Vår tjänst ersätter tidskrävande grävning och sparar energi. Vi mal ner stubben, skyddar gräsmattan och låter gräset återhämta sig snabbt. Hålet fylls igen och packas noggrant för en säker och estetiskt tilltalande trädgård.',
-            'image': 'img/Skärmbild 2023-03-27 142147.png',
-            'alt': 'Stubbfräsning'
-        },
-        'Buskfräsning': {
-            'description': 'Buskfräsning är en effektiv metod för att snabbt avlägsna oönskade buskar och häckar, skapa öppet utrymme och förändra trädgårdens utseende. Vi mal ner buskar och rötter med en kraftfull buskfräs, förhindrar återväxt och förbereder området för framtida trädgårdsprojekt, samtidigt som vi tar hänsyn till omgivande växter och träd. Vår buskfräsningstjänst förvandlar er trädgård till en attraktiv och funktionell plats på ett enkelt och effektivt sätt.',
-            'image': 'img/SC362_Action_01.jpg',
-            'alt': 'Buskfräsning'
-        },
-        'Trädfällning': {
-            'description': 'Trädfällning kan vara nödvändigt av flera skäl, såsom säkerhet, trädets hälsa eller för att skapa mer utrymme. Vi hanterar komplexa och riskfyllda uppgifter med erfaren personal, säkerhetsrutiner och rätt utrustning. Efter trädfällning erbjuder vi även bortforsling och stubbfräsning för att återställa området på ett säkert och effektivt sätt.',
-            'image': 'img/forest-g609f0d95e_1920.jpg',
-            'alt': 'Trädfällning'
+    /* Index for each container */
+    var divContainer = [
+        homeContainer,
+        aboutContainer,
+        contactContainer,
+        faqContainer
+    ];
+    /* Function: remove and add d-none from container associated with each nav button */
+    var shownContainer = function (index) {
+        var _a;
+        for (var _i = 0, divContainer_1 = divContainer; _i < divContainer_1.length; _i++) {
+            var container = divContainer_1[_i];
+            container === null || container === void 0 ? void 0 : container.classList.add('d-none');
         }
+        (_a = divContainer[index]) === null || _a === void 0 ? void 0 : _a.classList.remove('d-none');
     };
-    /* Loop to display content in array */
-    for (var key in homeArray) {
-        var serviceCard = createCard(key, homeArray[key]);
-        homeContainer.appendChild(serviceCard);
-    }
-    /* Append to container */
-    container.appendChild(homeContainer);
-};
-var aboutContent = function (container) {
-    /* About container */
-    var aboutContainer = document.createElement('div');
-    aboutContainer.classList.add('about-container', 'container');
-    /* Title */
-    var aboutTitle = document.createElement('h1');
-    aboutTitle.classList.add('about-title', 'text-center');
-    aboutTitle.textContent = 'Om oss';
-    var aboutText = document.createElement('p');
-    aboutText.classList.add('about-text');
-    aboutText.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque laudantium eius rerum, reiciendis voluptas quibusdam provident sint, qui ex, beatae assumenda error facilis vero fuga impedit quae dolorem blanditiis magni? Iusto nisi minima praesentium quis sint pariatur, recusandae architecto in omnis. Dignissimos repellendus quidem at cumque, ratione doloribus in dolorem illum nostrum. Recusandae repellendus obcaecati voluptatibus itaque quo! Enim, laboriosam?';
-    /* Append to container */
-    aboutContainer.appendChild(aboutTitle);
-    aboutContainer.appendChild(aboutText);
-    container.appendChild(aboutContainer);
-};
-var contactContent = function (container) {
-};
-var faqContent = function (container) {
-};
+    /*** Animations ***/
+    /* Inspiration from: https://www.google.com/search?client=safari&rls=en&q=javascript+tutorial+in+viewport&ie=UTF-8&oe=UTF-8 */
+    var section = document.querySelectorAll('section');
+    var withinViewport = function (el) {
+        var rect = el.getBoundingClientRect();
+        var offset = 100; // offset to show animation before element is 100% in viewport. looked better when scrolling
+        return (rect.top + offset >= 0 &&
+            rect.left + offset >= 0 &&
+            rect.bottom - offset <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right - offset <= (window.innerWidth || document.documentElement.clientWidth));
+    };
+    var animate = function () {
+        section.forEach(function (item) {
+            var article = item.querySelector('article');
+            /* Give each section its animation */
+            if (article && withinViewport(article)) {
+                if (article.classList.contains('bounceFromLeft')) {
+                    article.style.animation = 'bounceFromLeft 1s ease-out forwards';
+                }
+                else if (article.classList.contains('fadeIn')) {
+                    article.style.animation = 'fadeIn 1.5s ease-out forwards';
+                }
+                else if (article.classList.contains('bounceFromRight')) {
+                    article.style.animation = 'bounceFromRight 1s ease-out forwards';
+                }
+            }
+        });
+    };
+    /* Eventlistener for scroll */
+    window.addEventListener('scroll', animate);
+});
--- Projekt Dalastubben/script.ts
+++ Projekt Dalastubben/script.ts
@@ -1,156 +1,76 @@
 'use strict';
 
+/* Written in TypeScript */
+
 window.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.querySelector('.navbar') as HTMLElement;
-    const navbarOffsetTop = navbar.offsetTop;
-    const container = document.querySelector('.container') as HTMLElement;
-  
-    const toggleStickyNavbar = () => {
-        if (window.scrollY > navbarOffsetTop) {
-            document.querySelector('.hidden-nav')?.classList.remove('d-none');
-            navbar.classList.add('sticky');
-        } else {
-            document.querySelector('.hidden-nav')?.classList.add('d-none');
-            navbar.classList.remove('sticky');
-        }
-    };
-  
-    const setContent = (contentFunction: (container: HTMLElement) => void) => {
-        /* Back-to-top */
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        /* Empty content in container */
-        container.innerHTML = '';
-        /* Add default content to container */
-        contentFunction(container);
-    };
-  
-    /* Menu choices */
-    const contentMap: Record<string, (container: HTMLElement) => void> = {
-        'Hem': homeContent,
-        'Om oss': aboutContent,
-        'Kontakt': contactContent,
-        'FAQ': faqContent,
-    };
-  
-    /* Create eventlistener for sticky navbar */
-    window.addEventListener('scroll', toggleStickyNavbar);
-    /* Default site content */
-    homeContent(container);
-  
-    /* Create buttons */
-    document.querySelectorAll('.nav-btn').forEach((button) => {
-        button.addEventListener('click', (e) => {
-            const contentFunction = contentMap[(e.target as HTMLElement).textContent as keyof typeof contentMap];
-            if (contentFunction) {
-                setContent(contentFunction);
-            }
+    const navBtn = document.querySelectorAll('.nav-btn');
+    const homeContainer = document.querySelector('.home-container') as HTMLElement;
+    const aboutContainer = document.querySelector('.about-container') as HTMLElement;
+    const contactContainer = document.querySelector('.contact-container') as HTMLElement;
+    const faqContainer = document.querySelector('.faq-container') as HTMLElement;
+
+    /* Navbar buttons. */
+    navBtn.forEach( (button, index) => {
+        button?.addEventListener('click', () => {
+            navBtn.forEach( (otherButton) => {
+                if (otherButton !== button)
+                otherButton.classList.remove('active');
+            });
+            button?.classList.add('active');
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            shownContainer(index);
         });
     });
-});
 
-type Card = {
-    description: string;
-    image: string;
-    alt: string;
-};
+    /* Index for each container */
+    const divContainer = [
+        homeContainer,
+        aboutContainer,
+        contactContainer,
+        faqContainer
+    ];
 
-const createCard = (key: string, card: Card) => {
-    const Card = document.createElement('section');
-    Card.classList.add('card', 'm-3');
-
-    const CardBody = document.createElement('article');
-    CardBody.classList.add('card-body', 'scroll-item');
-
-    const CardImage = document.createElement('img');
-    CardImage.classList.add('card-img-top');
-    CardImage.src = card.image;
-    CardImage.alt = card.alt;
-
-    const CardTitle = document.createElement('h1');
-    CardTitle.classList.add('card-title');
-    CardTitle.textContent = key;
-
-    const CardText = document.createElement('p');
-    CardText.classList.add('card-text');
-    CardText.textContent = card.description;
-
-    CardBody.append(CardTitle, CardText);
-    Card.append(CardImage, CardBody);
-
-    return Card;
-};
-
-const homeContent = (container: HTMLElement) => {
-
-    /* Home container */
-    const homeContainer = document.createElement('div');
-    homeContainer.classList.add('home-container', 'container', 'scroll-container');
-
-    /* Array */
-    const homeArray: Record<string, Card> = {
-        'Stubbfräsning': {
-            'description': 
-                'Stubbfräsning hanterar effektivt besvärliga stubbar för jämnare gräsmattor och enklare robotgräsklipparinstallation. Vår tjänst ersätter tidskrävande grävning och sparar energi. Vi mal ner stubben, skyddar gräsmattan och låter gräset återhämta sig snabbt. Hålet fylls igen och packas noggrant för en säker och estetiskt tilltalande trädgård.', 
-            'image': 
-                'img/Skärmbild 2023-03-27 142147.png',
-            'alt':
-                'Stubbfräsning'
-        },
-        'Buskfräsning': {
-            'description':
-                'Buskfräsning är en effektiv metod för att snabbt avlägsna oönskade buskar och häckar, skapa öppet utrymme och förändra trädgårdens utseende. Vi mal ner buskar och rötter med en kraftfull buskfräs, förhindrar återväxt och förbereder området för framtida trädgårdsprojekt, samtidigt som vi tar hänsyn till omgivande växter och träd. Vår buskfräsningstjänst förvandlar er trädgård till en attraktiv och funktionell plats på ett enkelt och effektivt sätt.',
-            'image': 
-                'img/SC362_Action_01.jpg',
-            'alt':
-                'Buskfräsning'
-        },
-        'Trädfällning': {
-            'description': 
-                'Trädfällning kan vara nödvändigt av flera skäl, såsom säkerhet, trädets hälsa eller för att skapa mer utrymme. Vi hanterar komplexa och riskfyllda uppgifter med erfaren personal, säkerhetsrutiner och rätt utrustning. Efter trädfällning erbjuder vi även bortforsling och stubbfräsning för att återställa området på ett säkert och effektivt sätt.',
-            'image': 
-                'img/forest-g609f0d95e_1920.jpg',
-            'alt':
-                'Trädfällning'
+    /* Function: remove and add d-none from container associated with each nav button */
+    const shownContainer = (index: number) => {
+        for (const container of divContainer) {
+            container?.classList.add('d-none');
         }
-    };
-
-    /* Loop to display content in array */
-    for (const key in homeArray) {
-        const serviceCard = createCard(key, homeArray[key]);
-        homeContainer.appendChild(serviceCard);
+        divContainer[index]?.classList.remove('d-none');
     }
 
-    /* Append to container */
-    container.appendChild(homeContainer);
-};
+    /*** Animations ***/
+    /* Inspiration from: https://www.google.com/search?client=safari&rls=en&q=javascript+tutorial+in+viewport&ie=UTF-8&oe=UTF-8 */
+    const section = document.querySelectorAll('section');
 
-const aboutContent = (container: HTMLElement) => {
+    const withinViewport = (el: HTMLElement): boolean => {
+        const rect = el.getBoundingClientRect();
+        const offset = 100; // offset to show animation before element is 100% in viewport. looked better when scrolling
 
-    /* About container */
-    const aboutContainer = document.createElement('div');
-    aboutContainer.classList.add('about-container', 'container');
+        return (
+            rect.top + offset  >= 0 &&
+            rect.left + offset >= 0 &&
+            rect.bottom - offset <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right - offset <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
 
-    /* Title */
-    const aboutTitle = document.createElement('h1');
-    aboutTitle.classList.add('about-title', 'text-center');
-    aboutTitle.textContent = 'Om oss';
+    const animate = (): void => {
+        section.forEach( (item : Element) => {
+            const article = item.querySelector('article');
 
-    const aboutText = document.createElement('p');
-    aboutText.classList.add('about-text');
-    aboutText.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque laudantium eius rerum, reiciendis voluptas quibusdam provident sint, qui ex, beatae assumenda error facilis vero fuga impedit quae dolorem blanditiis magni? Iusto nisi minima praesentium quis sint pariatur, recusandae architecto in omnis. Dignissimos repellendus quidem at cumque, ratione doloribus in dolorem illum nostrum. Recusandae repellendus obcaecati voluptatibus itaque quo! Enim, laboriosam?';
+            /* Give each section its animation */
+            if (article && withinViewport(article)) {
+                if (article.classList.contains('bounceFromLeft')) {
+                    article.style.animation = 'bounceFromLeft 1s ease-out forwards';
+                } else if (article.classList.contains('fadeIn')) {
+                    article.style.animation = 'fadeIn 1.5s ease-out forwards';
+                } else if (article.classList.contains('bounceFromRight')) {
+                    article.style.animation = 'bounceFromRight 1s ease-out forwards';
+                }
+            }
+        });
+    };
 
-    /* Append to container */
-    aboutContainer.appendChild(aboutTitle);
-    aboutContainer.appendChild(aboutText);
-    container.appendChild(aboutContainer);
-}
-
-const contactContent = (container: HTMLElement) => {
-
-
-}
-
-const faqContent = (container: HTMLElement) => {
-
-
-}
+    /* Eventlistener for scroll */
+    window.addEventListener('scroll', animate);
+});

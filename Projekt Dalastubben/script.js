e3aca0a2e266a49cd23c9bea8ded3deb725f2cf0
speelgren <97473666+speelgren@users.noreply.github.com>
2023-05-05 03:02:25 +0200

    småfix

--- Projekt Dalastubben/script.js
+++ Projekt Dalastubben/script.js
@@ -1,82 +1,74 @@
 'use strict';
-/* Written in TypeScript */
-window.addEventListener('DOMContentLoaded', function () {
-    var container = document.querySelector('.scroll-container');
-    var navBtn = document.querySelectorAll('.nav-btn');
-    var homeContainer = document.querySelector('.home-container');
-    var aboutContainer = document.querySelector('.about-container');
-    var contactContainer = document.querySelector('.contact-container');
-    var faqContainer = document.querySelector('.faq-container');
-    /* Index for each container */
-    var divContainer = [
+
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.scroll-container');
+    const navBtns = document.querySelectorAll('.nav-btn');
+    const homeContainer = document.querySelector('.home-container');
+    const aboutContainer = document.querySelector('.about-container');
+    const contactContainer = document.querySelector('.contact-container');
+    const faqContainer = document.querySelector('.faq-container');
+    const sections = document.querySelectorAll('.scroll-item');
+
+    const divContainers = [
         homeContainer,
         aboutContainer,
         contactContainer,
         faqContainer
     ];
-    var scroll = function (targetContainer) {
+
+    const scrollToContainer = (targetContainer) => {
         container.scrollTo({
             top: targetContainer.offsetTop
         });
     };
-    /* Navbar buttons. */
-    navBtn.forEach(function (button, index) {
-        /* Eventlistener for each button */
-        button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-            /* Remove active class from other buttons */
-            navBtn.forEach(function (otherButton) {
-                /* Check if otherButton is not the same as button */
-                if (otherButton !== button)
+
+    navBtns.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            navBtns.forEach((otherButton) => {
+                if (otherButton !== button) {
                     otherButton.classList.remove('active');
+                }
             });
-            /* Add active class to button */
-            button === null || button === void 0 ? void 0 : button.classList.add('active');
-            shownContainer(index);
-            scroll(divContainer[index]);
+
+            button.classList.add('active');
+            displayContainer(index);
+            scrollToContainer(divContainers[index]);
         });
     });
-    /* Function: remove and add d-none from container associated with each nav button */
-    var shownContainer = function (index) {
-        var _a;
-        /* Add d-none to container associated with nav button */
-        for (var _i = 0, divContainer_1 = divContainer; _i < divContainer_1.length; _i++) {
-            var container_1 = divContainer_1[_i];
-            container_1 === null || container_1 === void 0 ? void 0 : container_1.classList.add('d-none');
-        }
-        /* Remove d-none to container associated with nav button */
-        (_a = divContainer[index]) === null || _a === void 0 ? void 0 : _a.classList.remove('d-none');
+
+    const displayContainer = (index) => {
+        divContainers.forEach((container) => {
+            container.classList.add('d-none');
+        });
+
+        divContainers[index].classList.remove('d-none');
     };
-    /*** Animations ***/
-    /* Inspiration from:
-     * https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
-     */
-    var section = document.querySelectorAll('.scroll-item');
-    /* Function: check if element is in viewport */
-    var isInViewport = function (element) {
-        var rect = element.getBoundingClientRect();
-        return (rect.top >= 0 &&
+
+    const isInViewport = (element) => {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
             rect.left >= 0 &&
             rect.bottom <= container.clientHeight &&
-            rect.right <= container.clientWidth);
+            rect.right <= container.clientWidth
+        );
     };
-    /* Function: animate element if it is in viewport */
-    var animate = function () {
-        section.forEach(function (item) {
-            var article = item.querySelector('article');
-            /* Give each section its animation */
+
+    const animateSections = () => {
+        sections.forEach((item) => {
+            const article = item.querySelector('article');
+
             if (article && isInViewport(article)) {
                 if (article.classList.contains('bounceFromLeft')) {
                     article.style.animation = 'bounceFromLeft 1s ease-out forwards';
-                }
-                else if (article.classList.contains('fadeIn')) {
+                } else if (article.classList.contains('fadeIn')) {
                     article.style.animation = 'fadeIn 1.5s ease-out forwards';
-                }
-                else if (article.classList.contains('bounceFromRight')) {
+                } else if (article.classList.contains('bounceFromRight')) {
                     article.style.animation = 'bounceFromRight 1s ease-out forwards';
                 }
             }
         });
     };
-    /* Eventlistener for scroll */
-    container.addEventListener('scroll', function () { return setTimeout(animate, 200); });
+
+    container.addEventListener('scroll', () => setTimeout(animateSections, 200));
 });
